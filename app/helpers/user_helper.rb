d6ed6fdfe2f77f7abd4ad104fa4ecc6d9a777689
Nick Noble <nick@miniware.team>
2024-03-01 16:14:09 -0500

    add a helper for profile links

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,6 @@
 module UserHelper
+  def profile_link_for user
+    username = (current_user && current_user == user) ? "You" : "@" + user.handle
+    link_to username, user, class: "font-bold hover:underline"
+  end
 end

032a3e1de8076c256a5f71d9c97efdf8707e31ef
Nick Noble <nick@miniware.team>
2024-03-04 16:16:41 -0500

    hotfix turbolinks

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
   def profile_link_for user
     username = (current_user && current_user == user) ? "You" : "@" + user.handle
-    link_to username, user, class: "font-bold hover:underline"
+    link_to username, user, class: "font-bold hover:underline", data: {turbo: false}
   end
 end
